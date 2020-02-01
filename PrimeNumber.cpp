7845bd13f464f2025da46585795da1903140e4a5
nitin12345-byte <60542129+nitin12345-byte@users.noreply.github.com>
2020-02-01 17:59:03 +0530

    a program for prime numbers

---
+++ PrimeNumber.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int prime(int num){
+
+  for(int i=2;i<num;i++
+    if(num%i==0)
+       return 0;
+       
+return 1;
+}
+
+
+int main(){
+
+  int num;
+  cout<<"enter anumber : ";
+  cin>>num;
+  
+  if(prime(num))
+   cout<<"Number is prime";
+  else
+   cout<<"Not prime";
+
+return 0;
+}
